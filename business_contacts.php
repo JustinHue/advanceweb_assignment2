04603cde5be97710daa6b60621b083bcd78eec20
JustinHue <justinhellsten@gmail.com>
2014-04-03 10:46:50 -0400

    Version 0.1
    Initial Commit with setup files for advance web assignment 2.
    Business Contacts page has been added to the main navigation on
    the left nav area. Created subdomain to contain assignment 2
    files on server. Database 'advanceweb' created.

---
+++ business_contacts.php
@@ -0,0 +1,32 @@
+<?php
+        /* 
+                File Name: business_contacts.php
+                Author's Name: Justin Hellsten
+                Website Name: www.justinhellsten.com
+                File Description: This is the login page for accessing business contacts
+		 		Date Last Modified: April 3rd 2014
+        */
+?>
+
+
+<?php
+        $left_index = 1;
+        include("includes/header.php");
+?>
+
+<div class="panel">
+	<div class="row">
+		<div class="columns">
+			<h1>List of Services</h1>
+			<h3>Game Development</h3>
+			<p>I offer services to develop and market games for android and iphone apps market. I program mostly with Java.</p>
+			<h3>Web Development + Design</h3>
+			<p>I charge $20/h for all  web development and design work but can vary depending on requirements. My skills for this arena are HTML, CSS, PHP, and MySQL. 
+		</p>
+	</div>
+</div>
+
+<?php
+        include("includes/bottom_include.php");
+?>
+
---
+++ includes/constants.php
@@ -0,0 +1,6 @@
+<?php
+	define("ROOT_DIR", "/advanceweb_assignment2/");
+	define("CSS_DIR", ROOT_DIR . "css/");
+	define("IMG_DIR", ROOT_DIR . "img/");
+	define("JS_DIR", ROOT_DIR . "js/");
+?>
